222de9cddfc76f5282af11835d773ceac4277130
Bryan Tiete Tanoue <bryan_tanoue@hotmail.com>
2021-01-28 14:14:12 -0300

    Programando os cactus

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event) { //responsável por capturar o usuário apertando "espaço"
     if(event.keyCode === 32) { //32 é o ódigo do espaço
-        jump();
+        if (!isJumping) {
+            jump();    
+        }
     }
 }
 
 function jump() { //faz o pulos
     let position = 0;
+
+    isJumping = true;
 
     //subindo
     let upInterval = setInterval(() => {
@@ -17,8 +23,10 @@ function jump() { //faz o pulos
             //descendo
             let downInterval = setInterval(() => {
                 if (position <= 0) {
+                    position = 20;
+                    dino.style.bottom = position + 'px'; //reseta na posição certa, ficava um pouco abixo do oriignal
+                    isJumping = false;
                     clearInterval(downInterval);
-                    dino.style.bottom =  + 'px'; //reseta na posição certa
                 }
                 
                 position -= 20;
@@ -29,8 +37,30 @@ function jump() { //faz o pulos
             dino.style.bottom = position + 'px';
         }   
     }, 20);
+}
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval (() => {
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus); //deleta o catus, evita processamento desnecessário
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    },20)
+
+    setTimeout(createCactus, randomTime); //recursividade, chama a funçaõ depois de um tempo aleátorio
 
 }
 
+createCactus();
 document.addEventListener('keyup', handleKeyUp); //adiciona o capturador de eventos
